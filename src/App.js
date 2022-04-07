4d1a2e9889e28c529f0510173d2d1d8b563ee322
Jenny Zonka <jennycade@gmail.com>
2022-04-06 18:19:08 -0700

    Attempting to test sign in form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 // import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import React from 'react';
 import { useState } from 'react';
 
 const App = () => {
-  const [heading, setHeading] = useState("Magnificent Monkeys");
-  // const [heading, setHeading] = useState('Malefecent monkeys');
+  // props
 
-  const clickHandler = () => {
-    setHeading("Radical Rhinos");
-  };
+  // state
+  const currentUser = useState({});
 
   return (
-    <>
-      <button type="button" onClick={clickHandler}>
-        Click Me
-      </button>
-      <h1>{heading}</h1>
-    </>
+    <div className='app'>
+      { (Object.keys(currentUser).length === 0) && (
+        <form>
+          <div className='mb-3'>
+            <label for="usernameInput" className='form-label'>Username</label>
+            <input type='text' className='form-control' id='usernameInput' />
+          </div>
+          <div className='mb-3'>
+            <label for="passwordInput" className='form-label'>Password</label>
+            <input
+              type='password'
+              className='form-control'
+              id='passwordInput'
+            />
+          </div>
+          <div className='mb-3'>
+            <button className='btn btn-primary'>Sign in</button>
+          </div>
+        </form>
+      )}
+    </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -4,20 +4,25 @@ import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('App component', () => {
-  it('renders magnificent monkeys', () => {
+  it('matches previous snapshot', () => {
     const { container } = render(<App />);
     expect(container).toMatchSnapshot();
   });
 
-  it('renders radical rhinos after button click', () => {
-    // get button to click
+  // it('renders radical rhinos after button click', () => {
+  //   // get button to click
+  //   render(<App />);
+  //   const button = screen.getByRole('button', { name: 'Click Me'});
+
+  //   // click it
+  //   userEvent.click(button);
+
+  //   // changes heading
+  //   expect(screen.getByRole('heading').textContent).toMatch(/radical rhinos/i);
+  // });
+
+  it('shows sign in form on load', () => {
     render(<App />);
-    const button = screen.getByRole('button', { name: 'Click Me'});
-
-    // click it
-    userEvent.click(button);
-
-    // changes heading
-    expect(screen.getByRole('heading').textContent).toMatch(/radical rhinos/i);
+    expect(screen.getByRole('form')).toBeInTheDocument();
   });
 });
