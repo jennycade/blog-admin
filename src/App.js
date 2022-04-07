78338360b89a26f2bcd5d0b5deb8cc1b18aba611
Jenny Zonka <jennycade@gmail.com>
2022-04-06 18:21:50 -0700

    Used useState correctly, duh

--- src/App.js
+++ src/App.js
@@ -8,18 +8,24 @@ const App = () => {
   // props
 
   // state
-  const currentUser = useState({});
+  const [currentUser, setCurrentUser] = useState({});
 
   return (
     <div className='app'>
       { (Object.keys(currentUser).length === 0) && (
         <form>
           <div className='mb-3'>
-            <label for="usernameInput" className='form-label'>Username</label>
+            <label
+              htmlFor="usernameInput"
+              className='form-label'
+            >Username</label>
             <input type='text' className='form-control' id='usernameInput' />
           </div>
           <div className='mb-3'>
-            <label for="passwordInput" className='form-label'>Password</label>
+            <label
+              htmlFor="passwordInput"
+              className='form-label'
+            >Password</label>
             <input
               type='password'
               className='form-control'
