91d2156abc41a90b663cddf866be6159a8bd1525
Jenny Zonka <jennycade@gmail.com>
2022-04-29 14:57:03 -0700

    Post correctly displays single post (no comments for now)

--- src/routes/Post.js
+++ src/routes/Post.js
@@ -4,12 +4,16 @@ import { useParams, Link } from 'react-router-dom';
 // DB
 import { getPost } from '../databaseFunctions';
 
+// components
+import Loading from '../Loading';
+
 function Post(props) {
   // props
   const { token } = props;
 
   // state
   const [post, setPost] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   // params
   const { postId } = useParams();
@@ -17,26 +21,33 @@ function Post(props) {
   // useEffect -> get post
   useEffect(() => {
     const fetchPost = async (postId, token) => {
+      setIsLoading(true);
       const newPost = await getPost(postId, token);
       setPost(newPost);
+      setIsLoading(false);
     };
     if (postId && token) {
       fetchPost(postId, token);
     }
-  }, [postId, token,]);
+  }, [postId, token]);
   
   return (
     <>
-      <h1>{post.title}</h1>
-      <p>{'Posted by '}
-        <Link to={`/users/${post.author._id}`}>{post.author.displayName}</Link>
-        {' on '}
-        <time dateTime={post.createdAt}>{post.createdAt}</time>
-      </p>
+    { isLoading || Object.keys(post).length === 0 ? <Loading /> : (
+      <>
+        <h1>{post.title}</h1>
+        <p>{'Posted by '}
+          <Link to={`/users/${post.author._id}`}>{post.author.displayName}</Link>
+          {' on '}
+          <time dateTime={post.createdAt}>{post.createdAt}</time>
+        </p>
 
-      <article>
-        {post.text}
-      </article>
+        <article>
+          {post.text}
+        </article>
+      </>)
+    }
+      
     </>
   );
 };
